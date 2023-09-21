0c1d226c68b6742e208285c16c14fbb5873c0d9e
Joao Victor <jvsdv3@gmail.com>
2023-09-21 20:55:43 -0300

    feat:estrutura

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,10 +1,12 @@
+//import { Navbar } from "@components/Navbar";
+
 const RootLayout = ({
     children,
   }: {
     children: React.ReactNode
   }) => {
     return (
-      <div className="flex items-center justify-center h-full">
+      <div className="md:pl-20 pt-16 h-full">
         <main>
         {children}
         </main>
