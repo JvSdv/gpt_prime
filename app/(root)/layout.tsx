911efc7f7b0ed17a1599552cef9f30545c1aaa2b
Joao Victor <jvsdv3@gmail.com>
2023-09-23 17:33:45 -0300

    feat: inicio do sidebar junto com search

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -9,10 +9,10 @@ const RootLayout = ({
     return (
       <div className="h-full">
         <Navbar />
-        <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
+        <div className="hidden md:flex mt-16 flex-col fixed inset-y-0">
           <Sidebar />
         </div>
-        <main className="md:pl-40 pt-16">
+        <main className="md:pl-64 pt-16">
           {children}
         </main>
       </div>
